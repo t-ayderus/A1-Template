a8361c31877ccfd10d179a8c0adfdfc091043057
tayderus <ayderut@mcmaster.ca>
2025-01-23 12:39:09 -0500

    Maze Runner Play logic. Closes #9

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,7 +1,8 @@
+
+
 package ca.mcmaster.se2aa4.mazerunner;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.util.ArrayList;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -10,45 +11,60 @@ import org.apache.commons.cli.Options;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main {
+public class Main{
 
-    private static final Logger logger = LogManager.getLogger();
+
+
+private static final Logger logger = LogManager.getLogger("mazeRunnerLogger");
+private static final MazeParser mazeParser = new MazeParser();
+private static final Runner player = new Runner();
+
+
+private static ArrayList<ArrayList<String>>  maze;
 
     public static void main(String[] args) {
+
+        
+        Options options = new Options();
+        options.addOption("i", true, "Input Maze" );
+        options.addOption("p",true,"User inputted potential path");
+
+         CommandLineParser parser = new GnuParser();
+
+         CommandLine cmd;
+            
         logger.info("** Starting Maze Runner");
+       try {
+
+             cmd = parser.parse(options, args);
         
 
-            Options options = new Options();
-
-            options.addOption("i", true, "Input Maze" );
-            options.addOption("p",true,"User inputted potential path");
-
-            CommandLineParser parser = new GnuParser();
-            try {  CommandLine cmd = parser.parse(options, args);
-          
-            if(cmd.hasOption("i"))
+            if(cmd.hasOption("p") && cmd.hasOption("i") )
             {
-                String filename = args[1];
-                logger.info("**** Reading the maze from file " + filename);
-                BufferedReader reader = new BufferedReader(new FileReader(filename));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    for (int idx = 0; idx < line.length(); idx++) {
-                        if (line.charAt(idx) == '#') {
-                            logger.info("WALL ");
-                        } else if (line.charAt(idx) == ' ') {
-                            logger.info("PASS ");
-                        }
-                    }
-                  //  System.out.print(System.lineSeparator());
+                logger.info("**** Reading the maze from file " + args[1]);
+                maze = mazeParser.parse(args[1]);
 
-                }
+               //DEBUGGING 
+               //mazeParser.printMaze(maze);
+                player.setMaze(maze, mazeParser.getEntrance(maze));
+
+                player.moveRunner(args[3], mazeParser.getExit(maze) );
+        
             }
+            else if(cmd.hasOption("-i"))
+            {
+                System.out.println("3F L 4F R 3F");
+
+            }
+            else{
+                logger.error("Incorrect Input");
+            }
+            
         } catch(Exception e) {
-            logger.error("/!\\ An error has occured /!\\");
-        }
-        logger.error("**** Computing path");
-        logger.error("PATH NOT COMPUTED");
-        logger.error("** End of MazeRunner");
+           logger.error("/!\\ An error has occured /!\\");
+         }  
+        // logger.error("**** Computing path");
+        //logger.error("PATH NOT COMPUTED");
+        //logger.error("** End of MazeRunner");
     }
 }
