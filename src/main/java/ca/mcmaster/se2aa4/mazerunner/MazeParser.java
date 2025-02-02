84f26329b44db5a756d549e2c694c24f2ae01de5
tayderus <ayderut@mcmaster.ca>
2025-02-02 13:30:45 -0500

    Fixed Bug with spaces and newlinws. Closes #8

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeParser.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeParser.java
@@ -4,6 +4,7 @@ package ca.mcmaster.se2aa4.mazerunner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,8 +13,15 @@ import org.apache.logging.log4j.Logger;
 public class MazeParser
 {
 
-    private static final Logger logger = LogManager.getLogger();
+    private final Logger logger = LogManager.getLogger();
 
+    public static void main(String[] args)
+    {
+        MazeParser mp = new MazeParser();
+        ArrayList<ArrayList<Path>>  maze = mp.parse(args[1]);
+        mp.printMaze(maze);
+
+    }
     
 
     //Finds Entrance of Maze
@@ -40,11 +48,29 @@ public class MazeParser
         }
         return null;
     }
-  
+    
+
+
+     public void printMaze(ArrayList<ArrayList<Path>> maze )
+    {        
+        Iterator<ArrayList<Path>>  listIterator= maze.iterator();
+
+        while(listIterator.hasNext())
+        {
+            ArrayList<Path> row = (ArrayList<Path>) listIterator.next();
+            for(Path col: row)
+            {
+                System.out.print( col +" ");
+            }
+            System.out.print(System.lineSeparator());
+        } 
+
+    }
+
     //Method to parse and return 2D representation of maze
     public ArrayList<ArrayList<Path>> parse(String filePath)
     {  
-           ArrayList<ArrayList<Path>> maze= new ArrayList<>();
+           ArrayList<ArrayList<Path>> maze= this.initializeMaze(filePath);
 
            try{
                 BufferedReader reader = new BufferedReader(new FileReader(filePath));
@@ -52,16 +78,10 @@ public class MazeParser
                 int row = 0;
                 while ((line = reader.readLine()) != null) {
 
-                    maze.add( new ArrayList<>() );
-
                     for (int idx = 0; idx < line.length(); idx++) {
                         if (line.charAt(idx) == '#') 
-                            maze.get(row).add(Path.WALL);
+                            maze.get(row).set(idx, Path.WALL);
 
-                        else if (line.charAt(idx) == ' ') 
-                            maze.get(row).add(Path.PASS);
-
-                        
                     }
                     row++;
             }
@@ -72,5 +92,45 @@ public class MazeParser
     
         return maze;
     }
+
+
+    public ArrayList<ArrayList<Path>> initializeMaze(String filePath)
+    {
+        int [] size = getSize(filePath);
+        int rows = size[0];
+        int length= size[1];     
+        ArrayList<ArrayList<Path>> maze = new ArrayList<>();
+            for(int i = 0; i<rows; i++)
+            {
+                maze.add(new ArrayList<>() );
+                for(int l = 0; l<length; l++)
+                {
+                    maze.get(i).add(Path.PASS);
+                }
+            }
+        return maze;
+    }
+
+
+    public int[] getSize(String filePath)
+    {
+        int rows = 0;
+        int length= 0;     
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            
+            while ((line = reader.readLine()) != null) {
+                if(rows ==0)
+                    length=line.length();
+                rows++;
+            }
+          } catch(Exception e) {
+           logger.error("/!\\ An error has occured /!\\\n");
+        }
+        return new int[] {rows,length};
+
+    }
+
 
 }
